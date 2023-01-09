e1f421ce89f6db7b9de9fd10e3c04f4ff5d75a72
Andy Rudnitsky <andy.rudnitsky@gmail.com>
2023-01-09 14:05:58 -0500

    First working version of both service and extension

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 import logging
-from flask import Flask, jsonify, request, Response, g
+import sys
+from flask import Flask, request
+from ukrainian_word_stress import Stressifier, StressSymbol
 
 app = Flask(__name__)
 app.logger.setLevel(logging.DEBUG)
 app.debug = True
+stressify = Stressifier(stress_symbol=StressSymbol.CombiningAcuteAccent)
+print("ukr-nlp-api available", file=sys.stderr)
+
 
 @app.route("/api/stress", methods=["POST"])
 def post_stress():
-
     jsonData = request.get_json()
-
-    stressedText = {"stressedtext": jsonData["text"].lower()}
-
+    # print("before", file=sys.stderr)
+    stressedText = {"stressedtext": stressify(jsonData["text"])}
+    # print(">", stressedText, file=sys.stderr)
     return stressedText
-
-@app.route("/")
-def get_root():
-    return "<body><p>Hello World!</p></body>"
